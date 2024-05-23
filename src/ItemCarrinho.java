c44f4ddcce37adf5f8c4c1d08061fb522cfd1edb
THASSO <thassokmorais@gmail.com>
2024-05-22 23:54:58 -0300

    Construindo produto e ItemCarrinho

--- src/ItemCarrinho.java
+++ src/ItemCarrinho.java
@@ -1,2 +1,23 @@
+import java.math.BigDecimal;
+
 public class ItemCarrinho {
+
+    private Produto produto;
+    private int quantidade;
+
+    public ItemCarrinho(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public BigDecimal total(){
+        return null;
+    }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,48 @@
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Produto {
+    private String codigo;
+    private String nome;
+    private BigDecimal preco;
+
+    public Produto(String codigo, String nome, BigDecimal preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(codigo, produto.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigo);
+    }
 }
