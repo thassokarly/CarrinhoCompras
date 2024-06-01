15e93fb700ca90957b24c7b827b3122ad439c176
THASSO <thassokmorais@gmail.com>
2024-05-31 23:17:43 -0300

    CarrinhoCompras

--- src/CarrinhoCompras.java
+++ src/CarrinhoCompras.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,4 +9,33 @@ public class CarrinhoCompras {
         items = new HashMap<>();
     }
 
+    public void adicionarProduto(Produto produto, int quantidade) {
+        ItemCarrinho item = new ItemCarrinho(produto, quantidade);
+        items.put(produto, item);
+    }
+
+    public ItemCarrinho getItem(String codProduto) {
+        for (ItemCarrinho item : items.values()) {
+            if (item.getProduto().getCodigo().equals(codProduto)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void excluirProduto(String codProduto) {
+        items.remove(getItem(codProduto).getProduto());
+    }
+
+    public int getQuantidades() {
+        return items.size();
+    }
+
+    public BigDecimal getTotal() {
+        BigDecimal total = BigDecimal.ZERO;
+        for (ItemCarrinho item : items.values()) {
+            total = total.add(item.getTotal());
+        }
+        return total;
+    }
 }
